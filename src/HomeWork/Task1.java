26332266cf54a2d260196f172ff3fa676a785ecd
Denis Azhymov <azhimov93@gmail.com>
2017-01-19 14:53:30 +0200

    init

---
+++ src/HomeWork/Task1.java
@@ -0,0 +1,48 @@
+package HomeWork;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by User on 19.01.2017.
+ */
+public class Task1 {
+
+    private static final File FILE = new File("D:\\practice\\Module11\\src\\testModule.txt");
+
+    public static void main(String[] args) {
+        Map<String, String> params = new HashMap<>();
+        params.put("1th", "Rings");
+        params.put("2nd", "Dark");
+        params.put("3rd", "throne");
+
+        System.out.println(Task1.replacer(params));
+    }
+    public static String replacer(Map<String, String> map) {
+        BufferedReader br = null;
+        StringBuilder replaced = new StringBuilder();
+        String line;
+
+        try {
+            br = new BufferedReader(new FileReader(FILE));
+            while ((line = br.readLine()) != null) {
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    line = line.replaceAll(entry.getKey(), entry.getValue());
+                }
+                replaced.append(line);
+                replaced.append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName());
+        }finally {
+            try {
+                if(br!=null)
+                    br.close();
+            }catch (IOException e){
+                System.out.println(e.getClass().getSimpleName());
+            }
+        }
+        String returned = replaced.toString();
+        return returned;
+    }
+}
